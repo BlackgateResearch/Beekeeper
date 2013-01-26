0789558605c3b6808f1e65cfaf7d42432a76b62a
Tristram Oaten <tristram@oaten.name>
2013-01-26 12:08:19 +0000

    fix

--- app.py
+++ app.py
@@ -17,29 +17,27 @@ redirect_uri = 'http://beekeeper.herokuapp.com/'
 
 
 @app.route('/')
-def hello():
+def index():
+    if request.args.get('code', False):
+        payload = {
+            'grant_type': 'authorization_code',
+            'code': request.args['code'],
+            'client_id': '981b4763b9ba42e888777a0c8d03e02b',
+            'client_secret': 'af6d9f6a6c684139b5a86fd6ee64ac31',
+            'redirect_uri': redirect_uri
+        }
+        r = requests.post("https://runkeeper.com/apps/token", data=payload)
+        return r.text
+    else:
+        args = {
+            'client_id': '981b4763b9ba42e888777a0c8d03e02b',
+            'response_type': 'code',
+            'redirect_uri': redirect_uri
+        }
 
-    args = {
-        'client_id': '981b4763b9ba42e888777a0c8d03e02b',
-        'response_type': 'code',
-        'redirect_uri': redirect_uri
-    }
+        url = furl('https://runkeeper.com/apps/authorize').add(args).url
+        return '<a href=%s>Authorise</a>' % url
 
-    url = furl('https://runkeeper.com/apps/authorize').add(args).url
-    return '<a href=%s>Authorise</a>' % url
-
-
-@app.route('/authorise/')
-def authorise():
-    payload = {
-        'grant_type': 'authorization_code',
-        'code': request.args['code'],
-        'client_id': '981b4763b9ba42e888777a0c8d03e02b',
-        'client_secret': 'af6d9f6a6c684139b5a86fd6ee64ac31',
-        'redirect_uri': redirect_uri
-    }
-    r = requests.post("https://runkeeper.com/apps/token", data=payload)
-    return r.text
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
