213fb307f561af53e998c6a7eddd7e5ba89c2596
Tristram Oaten <tristram@oaten.name>
2013-01-26 11:31:02 +0000

    added test

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
+# I dedicate any and all copyright interest in this software to the
+# public domain. I make this dedication for the benefit of the public at
+# large and to the detriment of my heirs and successors. I intend this
+# dedication to be an overt act of relinquishment in perpetuity of all
+# present and future rights to this software under copyright law.
+# unlicense.org
+
 import os
+
 from flask import Flask
+from furl import furl
 
 app = Flask(__name__)
 app.debug = True
@@ -7,7 +16,15 @@ app.debug = True
 
 @app.route('/')
 def hello():
-    return 'OK'
+
+    args = {
+        'client_id': '981b4763b9ba42e888777a0c8d03e02b',
+        'response_type': 'code',
+        'redirect_uri': 'http://beekeeper.herokuapp.com/'
+    }
+
+    url = furl('https://runkeeper.com/apps/authorize').add(args).url
+    return '<a href=%s>Authorise</a>' % url
 
 
 @app.route('/test/<value>')
